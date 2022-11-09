6eb4cdf14372e0cf9557fc095b0bc39013d95b62
Eliseo Carhuanca <Eliseo_Carhuanca@intranet.itresources>
2022-11-09 09:43:49 -0300

    initial

---
+++ server.js
@@ -0,0 +1,27 @@
+const io = require("socket.io")();
+
+const messageHandler = require("./handlers/message.handler");
+
+let currentUserId = 2;
+
+const users = {};
+
+io.on("connection", (socket) => {
+  console.log("socket.id:", socket.id);
+  users[socket.id] = { userId: currentUserId++ };
+  socket.on("join", (username) => {
+    users[socket.id].username = username;
+    console.log("username:", username);
+    socket.broadcast.emit("message", `${username} has joined the chat`);
+
+    messageHandler.handleMessage(socket, users);
+
+    // socket.on("disconnect", () => {
+    //   io.emit("message", `${username} has left the chat`);
+    // });
+  });
+
+  
+});
+
+io.listen(3001);
